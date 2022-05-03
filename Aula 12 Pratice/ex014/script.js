8994daf85f2e995c39671afcd0a2a3dde931f7d7
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-05-02 23:27:07 -0300

    melhorando hora do dia

--- Aula 12 Pratice/ex014/script.js
+++ Aula 12 Pratice/ex014/script.js
@@ -2,18 +2,20 @@ function carregar() {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-var hora = data.getHours()+'h'+ data.getMinutes()+'min'+ data.getSeconds()+'segundos'
+var hora = data.getHours()
+var minuto = data.getMinutes()
+var segundos = data.getSeconds()
 tempo = setInterval(carregar)
     if (hora >= 0 && hora < 12) {
-        msg.innerHTML = `<strong>Bom dia! São: ${hora}</strong>`
+        msg.innerHTML = `<strong>Bom dia! São: ${hora}h ${minuto}min ${segundos} segundos</strong>`
         img.src = 'manha.png'
         document.body.style.background = '#fdda45'
     } else if (hora >= 12 && hora < 18) {
-        msg.innerHTML = `<strong>Boa tarde! São: ${hora}</strong>`
+        msg.innerHTML = `<strong>Boa tarde! São: ${hora}h ${minuto}min ${segundos} segundos</strong>`
         img.src = 'tarde.png'
         document.body.style.background = '#d86615'
     } else {
-        msg.innerHTML = `<strong>Boa noite! São: ${hora}</strong>`
+        msg.innerHTML = `<strong>Boa noite! São: ${hora}h ${minuto}min ${segundos} segundos</strong>`
         img.src = 'noite.png'
         document.body.style.background = '#021623'
     }
