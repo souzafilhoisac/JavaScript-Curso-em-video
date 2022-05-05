135868dd1a6d35852c01d08f7c642cbfc0395ed2
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-05-05 10:53:49 -0300

    Exercicio Guanabara - 95%

--- Aula 14 Pratice/ex016/script1.js
+++ Aula 14 Pratice/ex016/script1.js
@@ -1,18 +1,22 @@
 function contar() {
-    var i = window.document.getElementById('i');
-    var f = window.document.getElementById('f');
-    var p = window.document.getElementById('p');
+    var i = window.document.getElementById('i').value;
+    var f = window.document.getElementById('f').value;
+    var p = window.document.getElementById('p').value;
     var res = window.document.querySelector('div#res');
-        if (i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
-            res.innerHTML = 'Impossível contar!'
-        } else if (p.value == 0) {
-            window.alert('Passo inválido! Considerando PASSO 1')
-            p += 1
+    if (i.length == 0 || f.length == 0 || p.length == 0) {
+        res.innerHTML = 'Impossível contar!'
     } else {
-        for (var s = i; s <= f; s += p) {
+        var numberP = Number(p);
+        if (numberP === 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            numberP += 1j
+        } else {
             var c = [];
-            c.push(i+=p)
-            res.innerHTML = `Contando:</br> ${c}`
+            const numberF = Number(f);
+            for (var s = Number(i); s <= numberF; s += numberP) {
+                c.push(s)
+            }
+            res.innerHTML = `Contando2:</br> ${c}`
         }
     }
 }
