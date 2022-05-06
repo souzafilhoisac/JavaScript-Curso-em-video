d29f8f9e5bb2a477b29d8a1a74c3f9e641acfce1
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-05-06 13:15:06 -0300

    Exercício Analisador de Numeros

---
+++ Aula 14 Pratice/ex018/script3.js
@@ -0,0 +1,12 @@
+function adicionar() {
+let num = Number(document.querySelector('div#txtn')).value
+let add = document.querySelector('div#adic')
+let finish = document.querySelector('div#fin')
+let fbox = document.querySelector('div#fullbox')
+}
+
+
+
+function finalizar() {
+
+}
