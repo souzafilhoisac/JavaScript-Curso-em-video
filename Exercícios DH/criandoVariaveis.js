53035902c8a2e7ac4f70e71515fadb16d081a6c0
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-05-05 22:09:37 -0300

    ExercíciosMod2

---
+++ Exercícios DH/criandoVariaveis.js
@@ -0,0 +1,4 @@
+/*Vamos escrever nossas primeiras variáveis juntos. Você irá usar tudo que aprendeu anteriormente! 
+Declare duas variáveis chamadas  idade  e  peso,  e atribua um valor numérico a ambas.*/
+var idade = 30
+var peso = 86
---
+++ Exercícios DH/criandoVariaveis2.js
@@ -0,0 +1,6 @@
+/*Agora para deixar todo esse conhecimento fixo na mente, precisamos praticar um pouco mais!
+Declare 4 variáveis: nome, sobrenome, numeroDaSorte e idade. Atribua um valor para cada uma delas, conforme o tipo.*/
+var nome = 'Isac'
+var sobrenome = 'Souza Filho'
+var numeroDaSorte = 16
+var idade = 30
