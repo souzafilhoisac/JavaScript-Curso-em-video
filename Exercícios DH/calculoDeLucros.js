d9b3d9b4a518d8ddaa353cad91de27d3015879da
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-05-06 00:42:04 -0300

    ExercíciosMod2

---
+++ Exercícios DH/calculoDeLucros.js
@@ -0,0 +1,12 @@
+/*Uma empresa separou os seus ganhos dos meses nas seguintes variáveis:
+let janeiro =  100
+let fevereiro = 300
+let março = 90
+Ela precisa da sua ajuda para saber o total do lucro adquirido. Sua missão é usar os operadores com as variáveis para realizar esse cálculo e use o console.log para imprimir o resultado!
+Para lhe auxiliarmos, já deixamos o código inicial:*/
+let janeiro = 100
+let fevereiro = 300
+let marco = 90
+
+let valorTotal = janeiro+fevereiro+marco
+console.log(valorTotal)
---
+++ Exercícios DH/cartaoDeVisitas.js
@@ -0,0 +1,7 @@
+/*Precisamos criar um código que gere cartão de visita com nome, sobrenome e profissão dos clientes, para isso:
+Crie uma variável para guardar cada informação (cada uma deve receber um dos 3 nomes acima), 
+Depois faça a concatenação das informações em uma nova variável chamada cartaoDeVisita.*/
+let nome = 'Taylor'
+let sobrenome = 'Alvarenga'
+let profissao = 'Programadora'
+let cartaoDeVisita = nome+' '+sobrenome+' - '+profissao
