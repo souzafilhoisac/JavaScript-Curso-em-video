fdc218996bfc6b4246261cea795957f3199da623
souzafilhoisac <souzafilhoisac@gmail.com>
2022-06-12 21:44:55 -0300

    Add Aula 16

--- Aula 16/funcao04.js
+++ Aula 16/funcao04.js
@@ -6,6 +6,26 @@ function fatorial(n) {
     return fat
 }
 
-console.log(fatorial(5))
+//console.log(fatorial(5))
      
 // 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+const total = function (A, B){
+    let somaTot = 0;
+    if(A < 0 || B < 0 || !B || typeof A !== 'number' || typeof B !== 'number'){
+        return 'Digite dois números inteiros positivos.'
+    } else if(A === B){
+        return A
+    } else if(A < B){
+        for (let i = A; i <= B; i++){
+            somaTot += i;
+        }
+    } else{
+        for (let i = B; i <= A; i++){
+            somaTot += i;
+        }
+    }
+    return parseInt(somaTot)
+};
+
+console.log(total(1.3, 3.5))
